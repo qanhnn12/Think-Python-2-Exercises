24da852a0ab6e53942c6f2a9207768ca2814340d
Anh Nguyen <qanhnn12@gmail.com>
2022-10-28 08:52:35 +0700

    shorten code

--- ch9_has_no_e.py
+++ ch9_has_no_e.py
@@ -14,6 +14,7 @@ def pct_no_e(filename):
         word = line.strip()
         total += 1
         if has_no_e(word):      # or use: if 'e' not in word
+            print(word)
             no_e_count += 1
 
     no_e_pct = no_e_count / total * 100
@@ -21,13 +22,4 @@ def pct_no_e(filename):
 
 
 if __name__ == '__main__':
-
-    # Display words that have no "e"
-    for line in open('words.txt'):
-        word = line.strip()
-        if has_no_e(word):
-            print(word)
-
-    # Percentage of words in the list that have no “e”
-    print()
     pct_no_e('words.txt')
