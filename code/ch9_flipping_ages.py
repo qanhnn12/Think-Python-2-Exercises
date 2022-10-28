b3a876eb1203fab3eab6275f6a587e69d799adc9
Anh Nguyen <qanhnn12@gmail.com>
2022-10-28 15:54:07 +0700

    add comment

--- code/ch9_flipping_ages.py
+++ code/ch9_flipping_ages.py
@@ -1,12 +1,35 @@
 def str_fill(i, n):
+    """
+    Returns i as a string with at least n digits.
+
+    :param i: int
+    :param n: int length
+    :return: string
+    """
     return str(i).zfill(n)
 
 
 def are_reversed(i, j):
+    """
+    Checks if i and j are the reverse of each other.
+
+    :param i: int
+    :param j: int
+    :return: boolean
+    """
     return str_fill(i, 2) == str_fill(j, 2)[::-1]
 
 
 def num_instances(diff, flag=False):
+    """
+    Counts the number of palindromic ages.
+
+    Returns the number of times the mother and daughter have
+    palindromic ages in their lives, given th difference in age.
+
+    :param diff: int difference in ages
+    :param flag: boolean, if True, prints the details
+    """
     daughter = 0
     count = 0
     while True:
@@ -25,6 +48,14 @@ def num_instances(diff, flag=False):
 
 
 def check_diffs():
+    """
+    Finds age difference that satisfy the problem.
+
+    Enumerate the possible differences in age between mother
+    and daughter, and for each difference, counts the number
+    of times over their lives they will have ages that are
+    the reverse of each other.
+    """
     diff = 10
     while diff < 70:
         n = num_instances(diff)
